943b31ee741d6efaac8ecab6a5a62012359d155b
Wagner Fonseca <wagner.fonseca@gmail.com>
2016-09-01 16:08:06 -0300

    controlando os cartões

--- cursos/material/exemplo 1/js/index.js
+++ cursos/material/exemplo 1/js/index.js
@@ -1,10 +1,6 @@
 $(function() {
     
-    $('.button-collapse').sideNav({
-        menuWidth: 300, // Default is 240
-        edge: 'right', // Choose the horizontal origin
-        closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
-        }
-    );
-    //$('.collapsible').collapsible();
+    $('.button-collapse').sideNav();
+    
+    $('.collapsible').collapsible();
 });
