325abebb8ac80a3433298d744a46391746f18d3a
wagner fonseca <wagner.fonseca@gmail.com>
2016-08-31 23:32:51 -0300

    Teste com Materialize

--- cursos/material/exemplo 1/js/index.js
+++ cursos/material/exemplo 1/js/index.js
@@ -1,3 +1,10 @@
 $(function() {
-    $('.collapsible').collapsible();
+    
+    $('.button-collapse').sideNav({
+        menuWidth: 300, // Default is 240
+        edge: 'right', // Choose the horizontal origin
+        closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
+        }
+    );
+    //$('.collapsible').collapsible();
 });
