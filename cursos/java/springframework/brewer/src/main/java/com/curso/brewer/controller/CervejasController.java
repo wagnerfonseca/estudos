3e8da0e0713a72811b6e1abfe20300bd4e90a497
Wagner Fonseca <wagner.fonseca@gmail.com>
2016-09-08 16:25:46 -0300

    Começando a alterar

--- cursos/java/springframework/brewer/src/main/java/com/curso/brewer/controller/CervejasController.java
+++ cursos/java/springframework/brewer/src/main/java/com/curso/brewer/controller/CervejasController.java
@@ -1,32 +1,28 @@
 package com.curso.brewer.controller;
 
-import java.util.Optional;
 
 import javax.validation.Valid;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.curso.brewer.model.Cerveja;
-import com.curso.brewer.repository.Cervejas;
+import com.curso.brewer.model.Sabor;
 
 @Controller
 public class CervejasController {
-	
-	@Autowired
-	private Cervejas cervejas;
-	
-	
-	@RequestMapping("/cervejas/novo")
-	public String novo(Cerveja cerveja) {
-		Optional<Cerveja> cervejaOpt = cervejas.findBySkuIgnoreCase("ASD");
-		System.out.println(cervejaOpt.isPresent());
 		
-		return "cerveja/CadastroCerveja";
+	@RequestMapping("/cervejas/novo")
+	public ModelAndView novo(Cerveja cerveja) {
+		ModelAndView mv = new ModelAndView("cerveja/CadastroCerveja");
+				
+		mv.addObject("sabores", Sabor.values());		
+		
+		return mv;
 	}
 	
 	@RequestMapping(value = "/cervejas/novo", method = RequestMethod.POST)
