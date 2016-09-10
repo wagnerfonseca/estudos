e6a6b855e2fdf8d5d411bbd3ce8a1feb4dbda9f3
wagner fonseca <wagner.fonseca@gmail.com>
2016-09-09 21:17:16 -0300

    Conversores de dados numericos e flutuantes. Forçando o padrao pt_BR para entrada de dados

--- cursos/java/springframework/brewer/src/main/java/com/curso/brewer/config/WebConfig.java
+++ cursos/java/springframework/brewer/src/main/java/com/curso/brewer/config/WebConfig.java
@@ -1,17 +1,23 @@
 package com.curso.brewer.config;
 
+import java.math.BigDecimal;
+import java.util.Locale;
+
 import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.format.number.NumberStyleFormatter;
 import org.springframework.format.support.DefaultFormattingConversionService;
 import org.springframework.format.support.FormattingConversionService;
+import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.i18n.FixedLocaleResolver;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.spring4.SpringTemplateEngine;
 import org.thymeleaf.spring4.templateresolver.SpringResourceTemplateResolver;
@@ -92,6 +98,20 @@ public class WebConfig extends WebMvcConfigurerAdapter implements ApplicationCon
 		// Converter de Estilo
 		conversionService.addConverter(new EstilosConverter());
 		
+		//Converter  BigDecimal
+		NumberStyleFormatter bigDecimalFomatter = new NumberStyleFormatter("#,##0.00"); //
+		conversionService.addFormatterForFieldType(BigDecimal.class, bigDecimalFomatter);
+		
+		// Converter numero inteiros (Informar o pattern no padrão internacional)
+		NumberStyleFormatter integerFormatter = new NumberStyleFormatter("#,##0");
+		conversionService.addFormatterForFieldType(Integer.class, integerFormatter);
+		
 		return conversionService;
+	}
+	
+	@Bean
+	public LocaleResolver localeResolver() {
+		// Força o Spring a sempre interpretar todas as entradas de dados com o Padrão pt-BR
+		return new FixedLocaleResolver(new Locale("pt", "BR"));
 	}
 }
