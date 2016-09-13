5f08575dcf02cc7fe8ad42dc0b5127c3a01bc197
wagner fonseca <wagner.fonseca@gmail.com>
2016-09-12 21:24:23 -0300

    Module pattern estilo cadastro rapido

--- cursos/java/springframework/brewer/src/main/resources/static/javascripts/estilo.cadastro-rapido.js
+++ cursos/java/springframework/brewer/src/main/resources/static/javascripts/estilo.cadastro-rapido.js
@@ -1,72 +1,80 @@
-$(function() {
-	
-	var modal = $("#modalCadastroRapidoEstilo");
-	var botaoSalvar = modal.find(".js-modal-cadastro-estilo-salvar-btn");
-	var form = modal.find("form");
-	var inputNomeEstilo = $("#nomeEstilo");
-	var containerMensagemErro = $(".js-mensagem-cadastro-rapido-estilo");
-	
-	// remover a ação de submit do form
-	form.on('submit', function(e) { e. preventDefault(); });
-	
-	// capturar a url de action
-	var url = form.attr('action');
-	
-	// Colocar foco no input 
-	// o shown.bs.modal, acontece depois que o modal é totalmente carregado
-	modal.on('shown.bs.modal', onModalShow);
-	// hide.bs.modal envendo no momento que fecha o modal 
-	modal.on('hide.bs.modal', onModalClose);
-	botaoSalvar.on('click', onBotaoSalvarClick);
+var Brewer = Brewer || {};
 
+Brewer.EstiloCadastroRapido = (function(){
+	
+	function EstiloCadastroRapido() {
+		// Colocar aqui todas as inicializações
+		// Todas as variaveis tornaram propriedades, atributos
+		this.modal = $("#modalCadastroRapidoEstilo");
+		this.botaoSalvar = this.modal.find(".js-modal-cadastro-estilo-salvar-btn");
+		this.form = this.modal.find("form");		
+		this.inputNomeEstilo = $("#nomeEstilo");
+		this.containerMensagemErro = $(".js-mensagem-cadastro-rapido-estilo");
+		// capturar a url de action
+		this.url = this.form.attr('action');
+	}
+	
+	EstiloCadastroRapido.prototype.iniciar = function() { // pode ser o nome da sua preferência		
+		// remover a ação de submit do form
+		this.form.on('submit', function(e) { e.preventDefault(); });	
+		// Colocar foco no input 
+		// o shown.bs.modal, acontece depois que o modal é totalmente carregado
+		this.modal.on('shown.bs.modal', onModalShow.bind(this)); // bind é para vincular com o contexto do objeto
+		// hide.bs.modal envendo no momento que fecha o modal 
+		this.modal.on('hide.bs.modal', onModalClose.bind(this));
+		this.botaoSalvar.on('click', onBotaoSalvarClick.bind(this));
+	}
 	
 	function onModalShow() {
-		inputNomeEstilo.focus();
+		this.inputNomeEstilo.focus();
 	}
 	
 	function onModalClose() {
-		inputNomeEstilo.val('');
-		containerMensagemErro.addClass('hidden');
-		form.find('.form-group').removeClass('has-error');
+		this.inputNomeEstilo.val('');
+		this.containerMensagemErro.addClass('hidden');
+		this.form.find('.form-group').removeClass('has-error');
 	}
 	
 	function onBotaoSalvarClick() {
-		var nomeEstilo = inputNomeEstilo.val().trim();
-		
+		var nomeEstilo = this.inputNomeEstilo.val().trim();		
 		$.ajax({
-			url: url,
+			url: this.url,
 			method: 'POST',
 			// padrao de dados de envio
 			contentType: 'application/json',
 			// Dados, valores que serão enviados para o metodo
 			data: JSON.stringify({ nome: nomeEstilo }),
 			// Erros
-			error: onErroSalvandoEstilo,
-			success: onEstiloSalvo
+			error: onErroSalvandoEstilo.bind(this),
+			success: onEstiloSalvo.bind(this)
 		});
 	}
-	
-	
+		
 	function onErroSalvandoEstilo(object) {
 		//Mostra na tela o objeto retornado em caso de erro ->  console.log(arguments);
-		var mensagemErro = object.responseText;
-		
-		containerMensagemErro.removeClass('hidden');
-		containerMensagemErro.html("<span>" +mensagemErro+ "</span>");
-		form.find('.form-group').addClass('has-error');
-		
+		var mensagemErro = object.responseText;		
+		this.containerMensagemErro.removeClass('hidden');
+		this.containerMensagemErro.html("<span>" +mensagemErro+ "</span>");
+		this.form.find('.form-group').addClass('has-error');		
 	}
 	
 	function onEstiloSalvo(estilo) { // pode ser qualquer nome		
-		// console.log(arguments); // Capturar o retorno
-		
+		// console.log(arguments); // Capturar o retorno		
 		var comboEstilo = $("#estilo");
 		comboEstilo.append("<option value=" + estilo.codigo + ">" + estilo.nome + "</option>");
-		comboEstilo.val(estilo.codigo);
-		
+		comboEstilo.val(estilo.codigo);		
 		// Esconder o Modal
-		modal.modal('hide');
-		
-	}
+		this.modal.modal('hide');		
+	}	
+	
+	return EstiloCadastroRapido;
+	
+}());
+
+
+$(function() {
+	
+	var estiloCadastroRapido = new Brewer.EstiloCadastroRapido();
+	estiloCadastroRapido.iniciar();			
 	
 });
