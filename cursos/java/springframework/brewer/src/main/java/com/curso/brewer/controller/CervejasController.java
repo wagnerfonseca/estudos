51bec45a69ae1d42becff57c7e98382e4b1d67c7
wagner fonseca <wagner.fonseca@gmail.com>
2016-09-20 23:19:48 -0300

    Inicio da pesquisa

--- cursos/java/springframework/brewer/src/main/java/com/curso/brewer/controller/CervejasController.java
+++ cursos/java/springframework/brewer/src/main/java/com/curso/brewer/controller/CervejasController.java
@@ -5,6 +5,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -13,19 +14,24 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import com.curso.brewer.model.Cerveja;
 import com.curso.brewer.model.Origem;
 import com.curso.brewer.model.Sabor;
+import com.curso.brewer.repository.Cervejas;
 import com.curso.brewer.repository.Estilos;
 import com.curso.brewer.service.CadastroCervejaService;
 
 @Controller
+@RequestMapping("/cervejas")
 public class CervejasController {
 	
 	@Autowired
 	private Estilos estilos;
-	
+		
 	@Autowired
 	private CadastroCervejaService service;
+	
+	@Autowired
+	private Cervejas cervejas;
 		
-	@RequestMapping("/cervejas/novo")
+	@RequestMapping("/novo")
 	public ModelAndView novo(Cerveja cerveja) {
 		ModelAndView mv = new ModelAndView("cerveja/CadastroCerveja");
 				
@@ -36,7 +42,7 @@ public class CervejasController {
 		return mv;
 	}
 	
-	@RequestMapping(value = "/cervejas/novo", method = RequestMethod.POST)
+	@RequestMapping(value = "/novo", method = RequestMethod.POST)
 	public ModelAndView cadastrar(@Valid Cerveja cerveja, BindingResult result, RedirectAttributes attributes) {
 		if (result.hasErrors()) {			
 			return novo(cerveja);
@@ -49,4 +55,19 @@ public class CervejasController {
 		return new ModelAndView("redirect:/cervejas/novo");
 	}
 	
+	/*
+	 * para pesquisa a boa prática é utilizar a requisicao GET
+	 * */
+	@GetMapping
+	public ModelAndView pesquisar() {
+		ModelAndView mv = new ModelAndView("cerveja/PesquisaCervejas");
+		
+		mv.addObject("sabores", Sabor.values());
+		mv.addObject("estilos", estilos.findAll());
+		mv.addObject("origens", Origem.values());
+		
+		mv.addObject("cervejas", cervejas.findAll());
+		
+		return mv;
+	}
 }
