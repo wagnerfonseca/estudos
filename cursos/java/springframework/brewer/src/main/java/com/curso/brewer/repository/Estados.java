24391b65c3e2075c45bd839b3746774457fccd53
wagner fonseca <wagner.fonseca@gmail.com>
2017-01-30 23:49:12 -0200

    16.6 - lookup(select) de estados

---
+++ cursos/java/springframework/brewer/src/main/java/com/curso/brewer/repository/Estados.java
@@ -0,0 +1,9 @@
+package com.curso.brewer.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.curso.brewer.model.Estado;
+
+public interface Estados extends JpaRepository<Estado, Long> {
+
+}
