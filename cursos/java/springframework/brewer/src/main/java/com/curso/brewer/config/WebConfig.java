eceddfda83c0a7baceab2b8e6d642bb618416bb0
wagner fonseca <wagner.fonseca@gmail.com>
2017-02-11 17:24:41 -0200

    18.05 - trabalhando com datas Bootstrap datepicker. traduzir mensagens usando o messages.properties

--- cursos/java/springframework/brewer/src/main/java/com/curso/brewer/config/WebConfig.java
+++ cursos/java/springframework/brewer/src/main/java/com/curso/brewer/config/WebConfig.java
@@ -1,6 +1,7 @@
 package com.curso.brewer.config;
 
 import java.math.BigDecimal;
+import java.time.format.DateTimeFormatter;
 import java.util.Locale;
 import java.util.concurrent.TimeUnit;
 
@@ -10,10 +11,13 @@ import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.guava.GuavaCacheManager;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
+import org.springframework.format.datetime.standard.DateTimeFormatterRegistrar;
 import org.springframework.format.number.NumberStyleFormatter;
 import org.springframework.format.support.DefaultFormattingConversionService;
 import org.springframework.format.support.FormattingConversionService;
@@ -129,6 +133,11 @@ public class WebConfig extends WebMvcConfigurerAdapter implements ApplicationCon
 		NumberStyleFormatter integerFormatter = new NumberStyleFormatter("#,##0");
 		conversionService.addFormatterForFieldType(Integer.class, integerFormatter);
 		
+		// API de Datas do Java 8
+		DateTimeFormatterRegistrar dateTimeFormatter = new DateTimeFormatterRegistrar();
+		dateTimeFormatter.setDateFormatter(DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+		dateTimeFormatter.registerFormatters(conversionService);
+		
 		return conversionService;
 	}
 	
@@ -156,5 +165,12 @@ public class WebConfig extends WebMvcConfigurerAdapter implements ApplicationCon
 		return cacheManager; 
 	}
 	
+	@Bean
+	public MessageSource messageSource() {
+		ReloadableResourceBundleMessageSource bundle = new ReloadableResourceBundleMessageSource();
+		bundle.setBasename("classpath:/messages"); //não precisa colocar .properties
+		bundle.setDefaultEncoding("UTF-8"); // 
+		return bundle;
+	}
 	
 }
