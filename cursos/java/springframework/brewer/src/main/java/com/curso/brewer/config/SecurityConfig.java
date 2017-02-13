7caac0be1b7e74a579312b3c3eecbf58a43cdc12
Wagner Fonseca <wagner.fonseca@gmail.com>
2017-02-13 16:02:07 -0300

    19.2 - Configurações básicas(iniciais) Spring security

--- cursos/java/springframework/brewer/src/main/java/com/curso/brewer/config/SecurityConfig.java
+++ cursos/java/springframework/brewer/src/main/java/com/curso/brewer/config/SecurityConfig.java
@@ -1,17 +1,46 @@
 package com.curso.brewer.config;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-@Configuration
-public class SecurityConfig {
+/*
+ * WebSecurityConfigurerAdapter proporciona alguns métodos para serem sobrescritos com relação a segurança 
+ * */
+
+@EnableWebSecurity // Ja tem a anotacao @Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth
+			.inMemoryAuthentication()
+				.withUser("admin")
+				.password("123456")
+				.roles("CADASTRO_CLIENTE");		
+	}
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+			.authorizeRequests()
+				.anyRequest().authenticated()
+				.and()
+			.formLogin()
+				.and()
+			.csrf().disable(); 
+	}
+	
 	
 	@Bean
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
+	
 	
 
 }
