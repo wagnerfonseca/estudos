f98f5bbdb5bf1986cca55e22a5324ab3ae766e7d
Wagner Fonseca <wagner.fonseca@gmail.com>
2017-03-23 10:31:13 -0300

    tipo booleano

---
+++ cursos/javascrip/curso_algaworks/tipo_boolean.js
@@ -0,0 +1,16 @@
+// 0 é false
+if (0) {
+	console.log("true");
+} else {
+	console.log("0 é false");
+}
+
+// como descrobir o valor boleando 
+
+console.log(!0);
+console.log(!!0);
+console.log(!1);
+console.log(!'');
+console.log(!!NaN);
+console.log(!!null);
+console.log(!!undefined);
