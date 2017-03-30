ded0adc4932fa34308a7f64ff11ec47967e2f5ff
Wagner Fonseca <wagner.fonseca@gmail.com>
2017-03-30 16:57:49 -0300

    alteracoes

--- workshop/javascript/module_pattern/module_export.js
+++ workshop/javascript/module_pattern/module_export.js
@@ -1,3 +1,5 @@
+
+
 // Global module
 var myModule = (function () {
  
@@ -19,3 +21,30 @@ var myModule = (function () {
 })();
 
 myModule.publicMethod();
+
+
+///---    
+var testModule = (function () {
+ 
+  var counter = 0;
+ 
+  return {
+ 
+    incrementCounter: function () {
+      return counter++;
+    },
+ 
+    resetCounter: function () {
+      console.log( "counter value prior to reset: " + counter );
+      counter = 0;
+    }
+  };
+ 
+})();
+ 
+// Usage: 
+// Increment our counter
+testModule.incrementCounter(); 
+// Check the counter value and reset
+// Outputs: counter value prior to reset: 1
+testModule.resetCounter();
