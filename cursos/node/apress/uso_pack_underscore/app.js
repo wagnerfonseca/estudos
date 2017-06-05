f330a4a11e0c81bf86a2872bc7466c54584ab28a
Wagner Fonseca <wagner.fonseca@gmail.com>
2017-06-05 14:06:28 -0300

    uso de programação funcional

--- cursos/node/apress/uso_pack_underscore/app.js
+++ cursos/node/apress/uso_pack_underscore/app.js
@@ -7,3 +7,20 @@ var results = _.filter(arr, function(item) {
 });
 
 console.log(results);
+
+// Passar por cada elemento do vetor e retorn aum novo vetor com o resultado
+var numeros = [1,3,7,12];
+var results1 = _.map(numeros, function(item) {
+    return item * 2;
+});
+
+console.log(results1);
+
+// Outro cenário em coleções obter  todos os elementos exceto aquele que não combina com a condição
+var nums = [0,1,2,3,4,5,6,7,8,9];
+
+var odds = _.reject(nums, function(n) {
+    return n % 2 == 0;
+});
+
+console.log(odds);
